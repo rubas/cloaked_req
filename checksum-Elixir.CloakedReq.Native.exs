3bd9110e0e63a0e8b4903e00930acf7389da3612
Dominic Rubas <1042243+rubas@users.noreply.github.com>
2026-06-11 16:44:48 +0200

    chore: sync v0.4.2 NIF checksum

--- checksum-Elixir.CloakedReq.Native.exs
+++ checksum-Elixir.CloakedReq.Native.exs
@@ -1,8 +1,8 @@
 %{
-  "libcloaked_req_native-v0.4.0-nif-2.17-aarch64-apple-darwin.so.tar.gz" =>
-    "sha256:7bbae414a5c0eb4ccfb62519012f136554ebe89d70250b56081b04af80393d78",
-  "libcloaked_req_native-v0.4.0-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:45ccc3d2c1f9be854b9a989cdccda4627e2f81d4417580a365b7bbad1c1f28e6",
-  "libcloaked_req_native-v0.4.0-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" =>
-    "sha256:33fbf0d7ff6ef91016a23430df90261ca29fb235c5001e37bcd76bb90df53046"
+  "libcloaked_req_native-v0.4.2-nif-2.17-aarch64-apple-darwin.so.tar.gz" =>
+    "sha256:beef113cff8c24c7e9a904584899e08994daf4a21979ef20dc07d942ac440010",
+  "libcloaked_req_native-v0.4.2-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:3efab5dc5e1fe88c9134a8aa68c3c7589d044be02e957b66854b200a0f59fa47",
+  "libcloaked_req_native-v0.4.2-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:5764b65c82fd044f9bbb231c9dbf57b8fb12c9d47c53ccc922b13327a1953d7b"
 }
